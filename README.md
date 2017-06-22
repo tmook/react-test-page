ab2b350b7f270f77aa56610ed6189d0cff010667
tmook <dev.tmook@hawaii.edu>
2017-06-22 11:13:57 -1000

    project setup

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# react-test-page
-
-## test table
-1. add
-2. delete
-
-## 3D components
-1. Unity WebGl
-2. Three.js
-
-## Graphing
-1. D3.js
-
-## Maps
-1. Google Maps
-
+# react tutorial v15.5
---
+++ js/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppTest from './AppTest.jsx';
+
+const element = <AppTest />;
+ReactDOM.render(
+   element,
+   document.getElementById('content')
+);

3ec0f1e77f0ffcd71948ed1e2c15d3cc33eacdf2
tmook <dev.tmook@hawaii.edu>
2017-06-26 02:57:03 -1000

    added dotenv package

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ var debug = process.env.NODE_ENV !== 'production';
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var Dotenv = require('dotenv-webpack');
 
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
   inject: 'body'
 });
+var DotenvConfig = new Dotenv({ path:'./.env' });
 
 
 module.exports = {
@@ -29,8 +31,9 @@ module.exports = {
          }
       ]
    },
-   plugins: debug ? [HTMLWebpackPluginConfig] : [
-      HTMLWebpackPluginConfig,
+  plugins: debug ? [HTMLWebpackPluginConfig, DotenvConfig] : 
+    [ HTMLWebpackPluginConfig,
+      DotenvConfig,
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.OccurrenceOrderPlugin(),
       new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
